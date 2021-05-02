b6881c9c258ede8b59ec1f386f09ea9632b560e7
Tohidul Islam <mdtohid.sust@gmail.com>
2021-05-03 00:47:07 +0600

    previous commit in a new flavour

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unused-state */
 import React from 'react';
 import ClickCounter from './Components/ClickCounter';
 import Counter from './Components/Counter';
@@ -8,35 +9,37 @@ export default class App extends React.Component {
     state = {
         // eslint-disable-next-line react/no-unused-state
         theme: 'light',
+        switchTheme: () => {
+            this.setState(({ theme }) => {
+                if (theme === 'dark') {
+                    return {
+                        theme: 'light',
+                    };
+                }
+                return {
+                    theme: 'dark',
+                };
+            });
+        },
+
         // eslint-disable-next-line react/no-unused-state
     };
 
-    switchTheme = () => {
-        this.setState(({ theme }) => {
-            if (theme === 'dark') {
-                return {
-                    theme: 'light',
-                };
-            }
-            return {
-                theme: 'dark',
-            };
-        });
-    };
-
     render() {
-        const { theme } = this.state;
         return (
-            <div className="app">
-                <Counter>
-                    {(counter, incrementCount) => (
-                        <ClickCounter count={counter} incrementCount={incrementCount} />
-                    )}
-                </Counter>
-                <ThemeContext.Provider value={{ theme, switchTheme: this.switchTheme }}>
-                    <Section />
-                </ThemeContext.Provider>
-            </div>
+            <ThemeContext.Provider value={this.state}>
+                <>
+                    <div className="app">
+                        <Counter>
+                            {(counter, incrementCount) => (
+                                <ClickCounter count={counter} incrementCount={incrementCount} />
+                            )}
+                        </Counter>
+
+                        <Section />
+                    </div>
+                </>
+            </ThemeContext.Provider>
         );
     }
 }
