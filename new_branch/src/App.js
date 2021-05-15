134b73af95dc4b10db3099a4d44988e7fc0934cc
Tohidul Islam <mdtohid.sust@gmail.com>
2021-05-16 03:23:01 +0600

    fetched data using useState and useeffect

--- new_branch/src/App.js
+++ new_branch/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [postId, setPostId] = useState("");
+  const [data, setData] = useState({});
+  const [buttonId, setButtonId] = useState("");
+  const handleClick = () => {
+    setButtonId(postId);
+  };
+  const handleChange = (e) => {
+    setPostId(e.target.value);
+  };
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${buttonId}`)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setData(res);
+      });
+  }, [buttonId]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>
+          <input type="text" value={postId} onChange={handleChange} />
+          <button type="button" onClick={handleClick}>
+            show title
+          </button>
+        </li>
+        <li>{data.title}</li>
+      </ul>
     </div>
   );
 }
